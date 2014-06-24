62fd2671e3194b7bdfc64c0fac1f30b286307d86
J.M <jm@jublo.net>
2014-06-24 13:58:58 +0200

    Shorten license header in source code file

--- src/shapecode.php
+++ src/shapecode.php
@@ -7,19 +7,8 @@
  * @version   1.1.0-dev
  * @author    Jublo Solutions <support@jublo.net>
  * @copyright 2014 Jublo Solutions <support@jublo.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @license   http://opensource.org/licenses/LGPL-3.0 GNU Lesser General Public License 3.0
+ * @link      https://github.com/jublonet/shapecode-php
  */
 
 /**
